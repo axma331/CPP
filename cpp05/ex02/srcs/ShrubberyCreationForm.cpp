f2187d3aaf7a97e0cf621d0cc3195015fdd82192
Fernanda Eschallots <feschall@oa-h3.msk.21-school.ru>
2022-02-22 07:44:55 +0300

    05/02 save

--- cpp05/ex02/srcs/ShrubberyCreationForm.cpp
+++ cpp05/ex02/srcs/ShrubberyCreationForm.cpp
@@ -4,4 +4,12 @@ ShrubberyCreationForm::ShrubberyCreationForm(const std::string& name, const int 
 	: Form(name, gradeToSign, gradeToExecute) {
 		std::cout << this->getGradeToSign();
 		std::cout << this->getGradeToExecute();
-	}
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(void) {}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other): Form(other) {}
+
+ShrubberyCreationForm&	ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other) {
+	Form::operator=(other);
+}
